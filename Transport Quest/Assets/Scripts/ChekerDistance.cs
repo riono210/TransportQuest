798a3c5d87e681de36197c73362e0fa76a824134
riono210 <e165729@ie.u-ryukyu.ac.jp>
2020-08-16 20:54:59 +0900

    回転と距離による判定を追加、速度変化処理も対応

---
+++ Transport Quest/Assets/Scripts/ChekerDistance.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChekerDistance : MonoBehaviour {
+
+    [SerializeField] private Transform needleMark;
+    [SerializeField] private Transform chekerMark;
+
+    [SerializeField] private RotationIncrease speedMater;
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown (KeyCode.Return)) {
+            DistanceComparison ();
+        }
+    }
+
+    // 二つのオブジェクトの距離を比較
+    public void DistanceComparison () {
+        float distance = Vector3.Distance (chekerMark.position, needleMark.position);
+        //Debug.Log ("distance: " + distance);
+        Judgment (distance);
+    }
+
+    // 距離から判定を出す
+    private void Judgment (float distance) {
+        float goodThresholdValue = 1f;
+        float excellentThresholdValue = 0.4f;
+
+        if (distance <= excellentThresholdValue) {
+            Debug.Log ("EXCELLENT!!!");
+            // 増やす
+
+            // スピードの調整
+            speedMater.SpeedAdjust (0.8f);
+        } else if (distance <= goodThresholdValue) {
+            Debug.Log ("good!");
+            speedMater.SpeedAdjust (0.3f);
+        } else {
+            Debug.Log ("bad...");
+            // スピードを一段階落とす
+            speedMater.SpeedAdjust (-1f);
+        }
+    }
+}
---
+++ Transport Quest/Assets/Scripts/RotationIncrease.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationIncrease : MonoBehaviour {
+
+    private Quaternion rotatorQuaternion;
+    private Vector3 rotatorEuler;
+    public float speed = 0.8f; // 角速度
+    public float speedFactor = 1f; // 速度倍率
+
+    // Start is called before the first frame update
+    void Start () {
+        rotatorQuaternion = this.transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        EulerRotation ();
+    }
+
+    private void EulerRotation () {
+        // 現在のQuaternionをオイラー角に変換
+        rotatorEuler = rotatorQuaternion.eulerAngles;
+
+        rotatorEuler = new Vector3 (rotatorEuler.x, rotatorEuler.y + (speed * speedFactor), rotatorEuler.z);
+
+        // vector3をquaternionに変換
+        rotatorQuaternion = Quaternion.Euler (rotatorEuler);
+        this.transform.localRotation = rotatorQuaternion;
+    }
+
+    public void SpeedAdjust (float speedDelta) {
+
+        if (speedDelta <= 0) { // batの場合
+            StartCoroutine (TemporarySlowdown (speedDelta));
+        } else {
+            speedFactor += speedDelta;
+        }
+
+        if (speedFactor <= 0.5f) {
+            speedFactor = 0.5f;
+        } else if (speedFactor >= 20f) {
+            speedFactor = 20f;
+        }
+    }
+
+    public IEnumerator TemporarySlowdown (float speedDelta) {
+        float tempSpeedFactor = speedFactor;
+        Debug.Log ("factor:" + tempSpeedFactor);
+        speedFactor = 0.3f;
+
+        yield return new WaitForSeconds (0.01f);
+
+        speedFactor = tempSpeedFactor + speedDelta;
+        if (speedFactor <= 0.5f) {
+            speedFactor = 0.5f;
+        }
+        Debug.Log ("aftor: " + speedFactor);
+    }
+}
